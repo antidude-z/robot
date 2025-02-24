ddf255842892bcf45aebd4ae24cfd721797461cf
Dee <dianakud20060127@gmail.com>
2025-02-24 14:14:18 +0500

    fix files

--- camera_output.py
+++ camera_output.py
@@ -1,20 +1,12 @@
 import cv2
 import numpy as np
-# RTSP-адрес 111
+# RTSP-адрес камеры
 rtsp_url = "rtsp://admin:UrFU_ISIT@10.32.9.223:554/Streaming/channels/101"
 
 # Создаем объект для захвата видео
 cap = cv2.VideoCapture(rtsp_url)
 
-K = np.array([[804.30675099, 0, 484.55601106],
-              [0, 820.81782668, 380.41727105],
-              [0, 0, 1]])  
-# Сама матрица параметров
-
-D = np.array([-5.02450741e+00,  2.70083432e+01, -1.17844515e-02, -2.55787550e-03]) 
-# Коэффициенты искажения (4 элемента) раньше было 5 но я убрал потому что были артефакты
-
-
+## Проверяем успешность подключения
 if not cap.isOpened():
     print("Ошибка: Не удалось подключиться к камере")
     exit()
@@ -27,11 +19,6 @@ try:
         if not ret:
             print("Ошибка: Не удалось получить кадр")
             break
-
-        h, w = frame.shape[:2]
-
-        new_K = cv2.fisheye.estimateNewCameraMatrixForUndistortRectify(K, D, (w, h), np.eye(3), balance=0.0)
-        undistorted_image = cv2.fisheye.undistortImage(image, K, D, Knew=new_K)
 
         frame = frame[86:873, 246:1033]
 
--- undistorted_camera_output.py
+++ undistorted_camera_output.py
@@ -1,23 +1,20 @@
 import cv2
 import numpy as np
-# RTSP-адрес камеры
+# RTSP-адрес 111
 rtsp_url = "rtsp://admin:UrFU_ISIT@10.32.9.223:554/Streaming/channels/101"
 
 # Создаем объект для захвата видео
 cap = cv2.VideoCapture(rtsp_url)
 
-#camera_matrix = np.array([[804.30675099, 0, 484.55601106],
-#                          [0, 820.81782668, 380.41727105],
-#                          [0, 0, 1]])
-#dist_coefs = np.array([-5.02450741e+00,  2.70083432e+01, -1.17844515e-02, -2.55787550e-03])
-#
-#size = (3000, 5000)
-#sizew = (1676, 846)
-#
-#newcameramtx, roi = cv2.getOptimalNewCameraMatrix(camera_matrix, dist_coefs, (size[0], size[1]), 1, (size[0], size[1]))
-#x, y, w, h = roi
-#n = 0
-## Проверяем успешность подключения
+K = np.array([[804.30675099, 0, 484.55601106],
+              [0, 820.81782668, 380.41727105],
+              [0, 0, 1]])  
+# Сама матрица параметров
+
+D = np.array([-5.02450741e+00,  2.70083432e+01, -1.17844515e-02, -2.55787550e-03]) 
+# Коэффициенты искажения (4 элемента) раньше было 5 но я убрал потому что были артефакты
+
+
 if not cap.isOpened():
     print("Ошибка: Не удалось подключиться к камере")
     exit()
@@ -31,16 +28,15 @@ try:
             print("Ошибка: Не удалось получить кадр")
             break
 
-        # cap.set(cv2.CAP_PROP_POS_MSEC,200)
-        # if ret:
-        #     cv2.imwrite(f"./images/frame{n}.png", frame) 
-        #     n += 1
+        h, w = frame.shape[:2]
 
-        #frame = cv2.undistort(frame, camera_matrix, dist_coefs, None, newcameramtx)
-        frame = frame[86:873, 246:1033]
+        new_K = cv2.fisheye.estimateNewCameraMatrixForUndistortRectify(K, D, (w, h), np.eye(3), balance=0.0)
+        undistorted_image = cv2.fisheye.undistortImage(frame, K, D, Knew=new_K)
+
+        # undistorted_image = undistorted_image[86:873, 246:1033]
 
         # Отображаем кадр в окне
-        cv2.imshow('IP Camera Stream', frame)
+        cv2.imshow('IP Camera Stream', undistorted_image)
 
         # Прерывание по нажатию 'q'
         if cv2.waitKey(1) & 0xFF == ord('q'):
